0d5c8d3b4588c0e6313b81e8b79029e35c74594a
Stefan Laimer <s.laimer@str8labs.com>
2022-10-03 00:26:11 +0200

    background stuff

--- Assets/_Scripts/ObjMover.cs
+++ Assets/_Scripts/ObjMover.cs
@@ -1,22 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ObjMover : MonoBehaviour
 {
+
     public Transform Start, End;
     public Vector2 SpeedRange;
 
+    private Transform _canvas;
     private float _speed;
 
     private void Awake()
     {
+        _canvas = GetComponentInParent<Canvas>().transform;
         _speed = Random.Range(SpeedRange.x, SpeedRange.y);
     }
 
     private void Update()
     {
-        transform.Translate(Vector3.right * _speed * Time.deltaTime);
+        transform.Translate(Vector3.right * _speed * _canvas.localScale.x * Time.deltaTime);
         if (transform.position.x > End.position.x)
         {
             ResetPos();
