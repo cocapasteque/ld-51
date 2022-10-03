70b3929981ffc308a11f98d88019f7d19ffba310
a.thiry <a.thiry@str8labs.com>
2022-10-03 12:27:23 +0200

    Saving Audio prefs in playerPrefs

--- Assets/_Scripts/AudioManager.cs
+++ Assets/_Scripts/AudioManager.cs
@@ -11,6 +11,13 @@ public class AudioManager : MonoBehaviour
     public Slider musicSlider;
     public Slider uiSlider;
     public Slider sfxSlider;
+
+    void Start()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("Music", 12);
+        uiSlider.value = PlayerPrefs.GetFloat("UI", -2);
+        sfxSlider.value = PlayerPrefs.GetFloat("Sfx", -2);
+    }
     
     // Update is called once per frame
     void Update()
@@ -18,5 +25,9 @@ public class AudioManager : MonoBehaviour
         mixer.SetFloat("Music", musicSlider.value);
         mixer.SetFloat("UI", uiSlider.value);
         mixer.SetFloat("Sfx", sfxSlider.value);
+        
+        PlayerPrefs.SetFloat("Music", musicSlider.value);
+        PlayerPrefs.SetFloat("UI", uiSlider.value);
+        PlayerPrefs.SetFloat("Sfx", sfxSlider.value);
     }
 }
